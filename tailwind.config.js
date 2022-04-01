2a8ad922fb50a52e6adfcb948721cf3021a97f82
DenilDenilson <jordanpreseleccion@gmail.com>
2022-04-01 14:57:12 -0500

    Proyecto configurado

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    screen: {
+      'xl': '1440px',
+    },
+    extend: {
+      colors: {
+        //### Primary
+
+        'Red': 'hsl(0, 100%, 68%)',
+
+        //### Neutral
+
+        'Very-Dark-Blue': 'hsl(230, 29%, 20%)',
+        'Dark-Grayish-Blue': 'hsl(230, 11%, 40%)',
+        'Grayish-Blue': 'hsl(231, 7%, 65%)',
+        'Light-Grayish-Blue': 'hsl(207, 33%, 95%)',
+      },
+
+      fontFamily: {
+        // font-family: 'Barlow', sans-serif;
+        // font-family: 'Barlow Condensed', sans-serif;
+        'Barlow': ['Barlow', 'sans-serif'],
+        'Barlow-Condensed': ['Barlow-Condensed', 'sans'],
+      },
+    },
   },
   plugins: [],
 }
